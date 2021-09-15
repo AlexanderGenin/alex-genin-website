6ea532402a0d57c7b4711d585aa7311b7927564c
Alex <55929549+AlexanderGenin@users.noreply.github.com>
2021-09-15 08:16:02 +0300

    Send Emails via email-js

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./common/heading";
 import Social from "./common/social";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { send } from "emailjs-com";
 
 export default function Contacts({ heading, text, socials }) {
+  const [toSend, setToSend] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      "service_otas5x5",
+      "template_7s80lxw",
+      toSend,
+      "user_Fq6lOqqCPVYKrluCiSCxl"
+    )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
   return (
     <section className="contacts" id="contacts">
       <div className="container">
@@ -28,15 +54,29 @@ export default function Contacts({ heading, text, socials }) {
             </div>
           </div>
           <div className="form">
-            <form action="#">
-              <input type="text" placeholder="Your name..." />
-              <input type="email" placeholder="Your email for reply..." />
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                placeholder="Your name..."
+                name="name"
+                value={toSend.name}
+                onChange={handleChange}
+              />
+              <input
+                type="email"
+                placeholder="Your email for reply..."
+                name="email"
+                value={toSend.email}
+                onChange={handleChange}
+              />
               <textarea
-                name=""
                 placeholder="Your message..."
                 id=""
                 cols="30"
                 rows="6"
+                name="message"
+                value={toSend.message}
+                onChange={handleChange}
               ></textarea>
               <button type="submit">
                 Send&nbsp;
